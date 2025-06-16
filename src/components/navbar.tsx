d8f960788af8fb97a27e2b3a5319bbfd07261798
harsh <harshsonegrahere@gmail.com>
2025-06-16 17:02:39 +0530

    fixed logs

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -24,26 +24,6 @@ import {
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-    const searchInput = (
-        <Input
-            aria-label="Search"
-            classNames={{
-                inputWrapper: "bg-default-100",
-                input: "text-sm",
-            }}
-            endContent={
-                <Kbd className="hidden lg:inline-block" keys={["command"]}>
-                    K
-                </Kbd>
-            }
-            labelPlacement="outside"
-            placeholder="Search..."
-            startContent={
-                <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-            }
-            type="search"
-        />
-    );
 
     return (
         <HeroUINavbar maxWidth="xl" position="sticky">
